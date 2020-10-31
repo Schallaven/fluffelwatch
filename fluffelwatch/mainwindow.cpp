7666178f7b762e59753c3b28466b04ab59f9b7ca
Sven Kochmann <sven.kochmann@gmx.de>
2020-10-31 13:13:05 -0400

    Borderless window and movement code

--- fluffelwatch/mainwindow.cpp
+++ fluffelwatch/mainwindow.cpp
@@ -3,12 +3,40 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    setStyleSheet("background-color: black; border: 1px solid red;");
+    setWindowFlags(Qt::FramelessWindowHint);
+    setAttribute(Qt::WA_TranslucentBackground, true);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent* event) {
+    /* Start moving the window with the left mouse button; this avoids accidently moving
+     * the window when accesing the context menu. */
+    if (event->button() == Qt::LeftButton) {
+        isMoving = true;
+        movingStartPos = event->pos();
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent* event) {
+    /* Moving the window: Calculating the difference between the current position of the
+     * mouse and the starting position. Then add this to the window position (top-left)
+     * corner to move the window the same distance/direction. */
+    if (isMoving) {
+        QPoint diffPos = event->pos() - movingStartPos;
+        window()->move(window()->pos() + diffPos);
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent* event) {
+    Q_UNUSED(event);
+
+    /* Stop moving in any case */
+    isMoving = false;
 }
--- fluffelwatch/mainwindow.h
+++ fluffelwatch/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMouseEvent>
 
 namespace Ui {
     class MainWindow;
@@ -15,8 +16,18 @@ class MainWindow : public QMainWindow
         explicit MainWindow(QWidget *parent = 0);
         ~MainWindow();
 
+        /* Mouse events */
+        void mousePressEvent(QMouseEvent* event) override;
+        void mouseMoveEvent(QMouseEvent* event) override;
+        void mouseReleaseEvent(QMouseEvent* event) override;
+
     private:
+        /* User interface definitions */
         Ui::MainWindow *ui;
+
+        /* Window movement on client */
+        bool isMoving = false;
+        QPoint movingStartPos;
 };
 
 #endif // MAINWINDOW_H
